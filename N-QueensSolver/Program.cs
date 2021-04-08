5c22c5f854d3fdbe2e684610fcf0392520b0b99d
Jhonatan Scremim <jhonatanscremim@gmail.com>
2021-04-07 21:51:01 -0300

    feat(create-project)

---
+++ N-QueensSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace N_QueensSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
