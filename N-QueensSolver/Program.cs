1217f47aef3fdc4902a361a259644aa664d55aae
Jhonatan Scremim <jhonatanscremim@gmail.com>
2021-04-10 17:28:56 -0300

    feat(apply-algorithm): aplica algoritmo

--- N-QueensSolver/Program.cs
+++ N-QueensSolver/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace N_QueensSolver
 {
@@ -6,10 +8,21 @@ namespace N_QueensSolver
     {
         static void Main(string[] args)
         {
-            bool[,] board = InitializeBoard(8);
-            DrawTheBoard(board);
-            Console.WriteLine("Total de colisões: " + CalculateCollisions(board));
-            Console.ReadKey();
+            for(int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Novo tabuleiro:");
+
+                bool[,] board = InitializeBoard(8);
+
+                DrawTheBoard(board);
+
+                int time = ApplySimulatedAnnealing(board, 35, 1.03, 35, 1.005);
+
+                DrawTheBoard(board);
+                Console.WriteLine(") Tempo decorrido: " + time + " ms");
+
+                Console.ReadKey();
+            }
         }
 
         #region Iniciar tabuleiro
@@ -29,7 +42,6 @@ namespace N_QueensSolver
                         board[i, j] = true;
                 }
             }
-
             return board;
         }
         #endregion
@@ -50,6 +62,8 @@ namespace N_QueensSolver
                 }
                 Console.WriteLine();
             }
+
+            Console.WriteLine("\n");
         }
 
         #endregion
@@ -104,5 +118,145 @@ namespace N_QueensSolver
         }
         #endregion
 
+        #region Algoritmo Simulated Annealing
+        static int ApplySimulatedAnnealing(bool[,] board, double initialTemp, double coolingFactor, double currentStabilizer, double stabilizingFactor)
+        {
+            int size = Convert.ToInt32(Math.Sqrt(board.Length));
+
+            Stopwatch stopwatch = new();
+
+            stopwatch.Start();
+
+            for (double temperature = initialTemp; (temperature > 0) && (CalculateCollisions(board) != 0); temperature -= coolingFactor)
+            {
+                Console.WriteLine("Temperatura : " + temperature + ", total de colisões: " + CalculateCollisions(board));
+                //DrawTheBoard(board);
+                for (int k = 0; k < currentStabilizer; k++)
+                {
+                    //bool[,] temporaryBoard = board.Clone() as bool[,];
+                    bool[,] temporaryBoard = new bool[size, size];
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            temporaryBoard[i, j] = board[i, j];
+                        }
+                    }
+                    Random rnd = new();
+
+                    //Primeiro faça um movimento aleatório
+                    int randomRowIndex;
+                    int randomColumnIndex;
+                    do
+                    {
+                        randomRowIndex = rnd.Next(8);
+                        randomColumnIndex = rnd.Next(8);
+                    }
+                    while (temporaryBoard[randomRowIndex, randomColumnIndex] == true && randomRowIndex == randomColumnIndex);
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (temporaryBoard[randomRowIndex, j] == true)
+                        {
+                            temporaryBoard[randomRowIndex, j] = false;
+                        }
+                    }
+                    temporaryBoard[randomRowIndex, randomColumnIndex] = true;
+
+                    //Então faça o melhor movimento
+                    int[,] successors = new int[size, size];
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        //Encontre a rainha da fileira
+                        int indexOfQueen = -1;
+                        for (int j = 0; j < size; j++)
+                        {
+                            if (temporaryBoard[i, j] == true)
+                            {
+                                indexOfQueen = j;
+                                temporaryBoard[i, j] = false;
+                            }
+                        }
+                        //Experimente todos os movimentos na linha e salve as colisões
+                        for (int j = 0; j < size; j++)
+                        {
+                            if (j != indexOfQueen)
+                            {
+                                temporaryBoard[i, j] = true;
+                                successors[i, j] = CalculateCollisions(temporaryBoard);
+                                temporaryBoard[i, j] = false;
+                            }
+                            else
+                            {
+                                successors[i, j] = 999; //Para ignorar a posição antiga
+                            }
+                        }
+                        temporaryBoard[i, indexOfQueen] = true; //Fixando a linha em sua primeira posição
+                    }
+
+                    //Selecione o menor valor de sucessores
+                    int min = 998;
+                    int indexI = -1;
+                    int indexJ = -1;
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            if (successors[i, j] < min)
+                            {
+                                min = successors[i, j];
+                                indexI = i;
+                                indexJ = j;
+                            }
+                        }
+                    }
+
+                    //Faça o movimento, remova a rainha da linha primeiro e coloque a nova
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (temporaryBoard[indexI, j] == true)
+                        {
+                            temporaryBoard[indexI, j] = false;
+                        }
+                    }
+                    temporaryBoard[indexI, indexJ] = true;
+
+                    //DrawTheBoard(temporaryBoard);
+
+
+                    double delta = CalculateCollisions(board) - CalculateCollisions(temporaryBoard);
+                    double probability = Math.Exp(delta / temperature);
+
+                    double rand = rnd.NextDouble();
+                    //Console.Clear();
+
+                    if (delta > 0)
+                    {
+                        for (int i = 0; i < size; i++)
+                        {
+                            for (int j = 0; j < size; j++)
+                            {
+                                board[i, j] = temporaryBoard[i, j];
+
+                            }
+                        }
+                    }
+                    else if (rand <= probability)
+                    {
+                        for (int i = 0; i < size; i++)
+                        {
+                            for (int j = 0; j < size; j++)
+                            {
+                                board[i, j] = temporaryBoard[i, j];
+                            }
+                        }
+                    }
+                }
+                currentStabilizer *= stabilizingFactor;
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed.Milliseconds;
+        }
+        #endregion
     }
 }
